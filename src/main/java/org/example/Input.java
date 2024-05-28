a800ee4c08742e2af47d88d0a5c44c96a05f1e61
ChrizDope <incubusid@googlemail.com>
2024-05-28 18:12:40 +0200

    Input class added

--- src/main/java/org/example/Input.java
+++ src/main/java/org/example/Input.java
@@ -3,31 +3,65 @@ package org.example;
 import java.util.Scanner;
 
 /**
- * Klasse, die den Input, sowie eine Instanz des Validators erzeugt
+ * Klasse, die den Input erzeugt
  */
 
 public  class Input {
 
     public Input(){
+        choice();
+    }
+
+    private void choice(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your password: ");
+        System.out.println("-----------------------------");
+        System.out.println("|   Validate password (1)   |");
+        System.out.println("|   Generate password (2)   |");
+        System.out.println("-----------------------------\n");
+        System.out.print("Please enter your choice: ");
+        String choice = sc.nextLine();
+
+        switch (choice) {
+             case "1":
+                 inputValidatePassword();
+                 break;
+             case "2":
+                 inputGeneratePassword();
+                 break;
+             default:
+                 System.out.println("\nInvalid choice");
+                 break;
+        }
+
+        sc.close();
+    }
+
+    private void inputValidatePassword(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nPlease enter your password: ");
         String password = sc.nextLine();
 
         PasswordValidator validator = new PasswordValidator(password);
 
         System.out.println(validator.validate());
 
+        sc.close();
+    }
 
-        System.out.println("\nPlease enter a password  length (>= 8): ");
+    private void inputGeneratePassword(){
+        Scanner sc = new Scanner(System.in);
+        PasswordValidator generator = new PasswordValidator();
+        System.out.print("\nPlease enter a password  length (>= 8): ");
+
 
         try {
             int length = sc.nextInt();
             if (length < 8) {
                 System.out.println("Your password should be at least 8 characters");
             } else {
-                if (validator.generatePassword(length)) {
-                    System.out.println("Your password was successfully generated");
-                    System.out.println("Your generated Password: " + validator.getGeneratedPassword());
+                if (generator.generatePassword(length)) {
+                    System.out.println("\nYour password was successfully generated");
+                    System.out.println("Your generated Password: " + generator.getGeneratedPassword());
                 }
             }
         }
